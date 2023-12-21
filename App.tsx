66533d34ce1f59db51cf391624ee3705df05d490
Matheus <matheustorresdev@gmail.com>
2023-12-21 18:44:03 -0300

    Estrutura Inicial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  useFonts,
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+} from "@expo-google-fonts/nunito-sans";
+import { ActivityIndicator,  View } from "react-native";
+import { Home } from "@screens/Home";
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts([NunitoSans_400Regular, NunitoSans_700Bold]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
+      {fontsLoaded ? (
+        <Home />
+      ) : (
+        <ActivityIndicator color="#000" size="large" />
+      )}
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Screen</Text>
+    </View>
+  );
+};
