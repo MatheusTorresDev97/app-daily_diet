0111ad5b4d905c2c87511b15fb9245ca5d654584
Matheus <matheustorresdev@gmail.com>
2023-12-22 12:45:25 -0300

    criando componente de section list

--- src/components/Meal/index.tsx
+++ src/components/Meal/index.tsx
@@ -1,25 +1,27 @@
 import { Typography } from "@components/Typography";
 import { Container, Divider, Status } from "./styles";
+import { PressableProps } from "react-native";
 
-export type MealStatus = {isOnTheDiet: boolean}
+export type MealStatus = { isOnTheDiet: boolean };
 
-type MealProps = MealStatus & {
-  id: string,
-  hour: string,
-  description: string
-}
+type MealProps = PressableProps &
+  MealStatus & {
+    id: string;
+    hour: string;
+    description: string;
+  };
 
-export const Meal = ({id, hour, description, isOnTheDiet}: MealProps) => {
+export const Meal = ({ ...props }: MealProps) => {
   return (
-    <Container id={id}>
+    <Container {...props}>
       <Typography fontSize="body_xs" family="bold">
-        {hour}
+        {props.hour}
       </Typography>
       <Divider />
       <Typography style={{ flex: 1 }} numberOfLines={1}>
-       {description}
+        {props.description}
       </Typography>
-      <Status isOnTheDiet={isOnTheDiet}/>
+      <Status isOnTheDiet={props.isOnTheDiet} />
     </Container>
   );
 };
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,13 +2,37 @@ import { Container } from "./styles";
 
 import { Header } from "@components/Header";
 import { Card } from "@components/Card";
-import { View } from "react-native";
+import { View, SectionList } from "react-native";
 import { Typography } from "@components/Typography";
 import { Button } from "@components/Button";
 import { Plus } from "phosphor-react-native";
 import { Meal } from "@components/Meal";
 
 export const Home = () => {
+  const meals = [
+    {
+      title: "22.08.23",
+      data: [
+        {
+          id: "01",
+          hour: "20:00",
+          description: "X-tudo",
+          isOnTheDiet: false,
+        },
+      ],
+    },
+    {
+      title: "22.12.23",
+      data: [
+        {
+          id: "02",
+          hour: "21:00",
+          description: "Salada",
+          isOnTheDiet: true,
+        },
+      ],
+    },
+  ];
   return (
     <Container>
       <Header />
@@ -25,20 +49,50 @@ export const Home = () => {
           Refeições
         </Typography>
 
-        <Button 
-        title="Nova refeição" 
-        icon={<Plus color="white" size={18} />} 
-        isActive
-        onPress={() => console.log('Nova refeição')}
-        type="solid"
+        <Button
+          title="Nova refeição"
+          icon={<Plus color="white" size={18} />}
+          isActive
+          onPress={() => console.log("Nova refeição")}
+          type="solid"
         />
       </View>
 
-      <Meal 
-      id="01"
-      hour="20:00"
-      description="X-Tudo"
-      isOnTheDiet={false}
+      <SectionList
+        sections={meals}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Meal
+            key={item.id}
+            {...item}
+            onPress={() => console.log(item.description)}
+          />
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Typography
+            fontSize="title_sm"
+            family="bold"
+            color="gray_1"
+            style={{ marginBottom: 8, marginTop: 8 }}
+          >
+            {title}
+          </Typography>
+        )}
+        style={{ marginBottom: 32 }}
+        contentContainerStyle={
+          meals.length === 0 && {
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+          }
+        }
+        ListEmptyComponent={() => (
+          <>
+            <Typography>Não há refeições cadastradas ainda.</Typography>
+            <Typography>Vamos cadastrar refeições hoje?</Typography>
+          </>
+        )}
+        showsVerticalScrollIndicator={false}
       />
     </Container>
   );
